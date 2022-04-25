ef74690e043b90e85af8bcac4532257baf65ed5d
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-04-25 17:17:19 +0900

    QueryString & Navigate 사용하기

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,34 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+  console.log(id, mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ id: "200", mode: "black" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
