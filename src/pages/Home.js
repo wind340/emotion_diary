baa45da2b9bd70ef4950193efff2e6de2dfdc06d
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-04-27 00:20:29 +0900

    home detail

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,60 @@
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+import DiaryList from "./../components/DiaryList";
+
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  // 데이터 가공해서 관리
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()} 년 ${curDate.getMonth() + 1}월`;
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+      console.log(firstDay);
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      //필터를 이용해 list에 들어있는 월별로 지정하기
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>이곳은 홈 입니다.</p>
+    <div className="DiaryHome">
+      <div>
+        <MyHeader
+          headText={headText}
+          leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+          rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+        />
+        <DiaryList />
+      </div>
     </div>
   );
 };
