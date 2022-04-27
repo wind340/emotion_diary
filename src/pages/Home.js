68538abf3bdc0286aa04f1dd27562c8bcfd1ad4e
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-04-27 23:53:41 +0900

    data pipe

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -53,7 +53,7 @@ const Home = () => {
           leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
           rightChild={<MyButton text={">"} onClick={increaseMonth} />}
         />
-        <DiaryList />
+        <DiaryList diaryList={data} />
       </div>
     </div>
   );
