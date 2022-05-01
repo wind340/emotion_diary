12b449ff8ac74a8ca8b5005141fae79cd77c1ecf
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-02 01:29:40 +0900

    title 변환 함수 추가

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -12,6 +12,12 @@ const Diary = () => {
   const diaryList = useContext(DiaryStateContext);
   const navigate = useNavigate();
   const [data, setData] = useState();
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기`;
+  }, []);
+
   useEffect(() => {
     if (diaryList.length >= 1) {
       const targetDiary = diaryList.find(
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,12 @@ const Home = () => {
   const [data, setData] = useState([]);
   const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()} 년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - 새 일기`;
+  }, []);
+
   const decreaseMonth = () => {
     setCurDate(
       new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
